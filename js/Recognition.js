4ca0f5e4f9350e794e62cd591320a82ef84b419c
Zohaldon <parth.pacs@gmail.com>
2019-04-25 22:18:19 -0400

    version 1.0
    initial commit

---
+++ js/Recognition.js
@@ -0,0 +1,54 @@
+//When Window is loaded
+window.addEventListener("DOMContentLoaded",	() => {
+    
+    //Configure Speech Recogniton
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition(),
+    RecognitonOutput = document.querySelector('.Recognition-Output');
+
+    var Winter = document.getElementById("w");
+    var Summer = document.getElementById("s");
+
+    function ChangeSeasonToSummer()
+    {
+        Winter.style.display='none';
+        Summer.style.display='block';
+    }
+
+    function ChangeSeasonToWinter()
+    {
+        Winter.style.display='block';
+        Summer.style.display='none';
+    }
+
+    //Initiate Speech Recognition
+    recognition.start();
+
+    //Listen uptill user finishes commanding
+    recognition.addEventListener('result', e => {
+
+        //Get the Trnscript of User's Speech
+        //Removes all of the Spaces
+        const transcript = e.results[0][0].transcript.toLowerCase().replace(/\s/g, '');
+
+        //Print The recognised text
+        RecognitonOutput.textContent = transcript;
+
+        //Check if Recived Transcript could be applied to change the season or not 
+        if (transcript == 'winter')
+        {
+            ChangeSeasonToWinter();
+        }
+        else
+        {
+            if(transcript == 'summer')
+            {
+                ChangeSeasonToSummer();
+            }
+        }
+    });
+
+    //Restart speech recognition after user has finished talking
+    recognition.addEventListener('end', recognition.start);
+});
